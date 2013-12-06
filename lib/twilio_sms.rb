e7f8adda6888d2c31cc9c949c33108c09412447d
audeemac <amvelasco@gmail.com>
2013-12-05 16:34:54 -0800

    not hardcoded numbers

--- lib/twilio_sms.rb
+++ lib/twilio_sms.rb
@@ -2,15 +2,16 @@ require "twilio_sms/version"
 require "twilio-ruby"
 
 module TwilioSms
-	class self.initialize
+    class self.initialize
   		client = Twilio::REST::Client.new account_sid, auth_token
   	end
 
   	class self.text(message, options={})
-  		client.account.messages.create(
-  			:from => '+14159341234',
-  			:to => '+16105557069',
-  			:body => 'Hey there!')
-  		
+  		res = client.account.messages.create(
+  			:from => options[:to],
+  			:to => options[:from],
+  			:body => message 
+  		)
+      puts res
   	end
 end
